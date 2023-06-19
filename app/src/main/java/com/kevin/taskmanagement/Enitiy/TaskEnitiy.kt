119b548bf42e8f464ea4e02b98f0b4b591b28b18
monul <monu.lakhara2004@gmail.com>
2023-06-19 15:18:02 +0530

    commit_2

--- app/src/main/java/com/kevin/taskmanagement/Enitiy/TaskEnitiy.kt
+++ app/src/main/java/com/kevin/taskmanagement/Enitiy/TaskEnitiy.kt
@@ -7,7 +7,13 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "task")
 data class TaskEnitiy(
 
-    @ColumnInfo(name = "Task") var Task: String
+    @ColumnInfo(name = "title") var title: String,
+    @ColumnInfo(name = "text") var text: String,
+    @ColumnInfo(name = "date") var date: String,
+    @ColumnInfo(name = "month") var month: String,
+    @ColumnInfo(name = "year") var year: String,
+    @ColumnInfo(name = "hour") var hour: String,
+    @ColumnInfo(name = "minute") var minute: String,
 ) {
     @PrimaryKey(autoGenerate = true) var id:Int = 0
 }
--- app/src/main/java/com/kevin/taskmanagement/MainActivity.kt
+++ app/src/main/java/com/kevin/taskmanagement/MainActivity.kt
@@ -2,11 +2,37 @@ package com.kevin.taskmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.kevin.taskmanagement.Fragment.AddTaskFragment
+import com.kevin.taskmanagement.Fragment.HomeFragment
+import com.kevin.taskmanagement.Fragment.ListFragment
+import com.kevin.taskmanagement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+        binding.bottom.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.Home -> replaceFragment(HomeFragment())
+                R.id.Add -> replaceFragment(AddTaskFragment())
+                R.id.List -> replaceFragment(ListFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragpageview,fragment).commit()
 
     }
 }
