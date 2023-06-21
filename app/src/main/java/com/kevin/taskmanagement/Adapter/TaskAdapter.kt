55e7dcf4e00a318816a1d9af6af3bd405cee1c55
kevin-monapara89 <kevinmonapara89@gmail.com>
2023-06-21 22:22:21 +0530

    commit k

--- app/src/main/java/com/kevin/taskmanagement/Adapter/TaskAdapter.kt
+++ app/src/main/java/com/kevin/taskmanagement/Adapter/TaskAdapter.kt
@@ -1,59 +1,36 @@
 package com.kevin.taskmanagement.Adapter
 
-import android.annotation.SuppressLint
-import android.app.DatePickerDialog
-import android.app.Dialog
-import android.app.TimePickerDialog
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.DatePicker
-import android.widget.TimePicker
-import android.widget.Toast
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.kevin.taskmanagement.Database.RoomDB
 import com.kevin.taskmanagement.Enitiy.TaskEnitiy
 import com.kevin.taskmanagement.databinding.TodolistviewBinding
-import com.kevin.taskmanagement.databinding.UpdatedialogBinding
-import java.text.SimpleDateFormat
-import java.util.Date
 import java.util.Locale
+import java.util.logging.Filter
 
-class TaskAdapter(
-    list: ArrayList<TaskEnitiy>
-) : RecyclerView.Adapter<TaskAdapter.TaskHolder>() {
-    lateinit var context: Context
-    var list = list
-    lateinit var taskEnitiy :TaskEnitiy
-    lateinit var db: RoomDB
+class TaskAdapter(getTask: ArrayList<TaskEnitiy>) : RecyclerView.Adapter<TaskAdapter.TaskHolder>(){
 
+    var getTask = getTask
+    private var filteredList: MutableList<TaskEnitiy> = getTask.toMutableList()
 
     class TaskHolder(itemView: TodolistviewBinding) : ViewHolder(itemView.root) {
-
         var binding = itemView
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskHolder {
-        var binding =
-            TodolistviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-
-        var binding =
-            TodolistviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        context = parent.context
-
-
+        var binding = TodolistviewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return TaskHolder(binding)
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return getTask.size
     }
 
-    override fun onBindViewHolder(holder: TaskHolder, @SuppressLint("RecyclerView") position: Int) {
-        db = RoomDB.init(context).task()
+    override fun onBindViewHolder(holder: TaskHolder, position: Int) {
         holder.binding.apply {
-            list.get(position).apply {
+            getTask.get(position).apply {
                 txttitle.text = title
                 txtdiscription.text = discription
                 txtdate.text = date
@@ -61,124 +38,18 @@ class TaskAdapter(
                 txtdate.text = year
                 txttime.text = hour
                 txttime.text = minute
-
-            }
-        }
-        holder.binding.imgubdata.setOnClickListener {
-
-            Toast.makeText(context, "Hiii....", Toast.LENGTH_SHORT).show()
-
-            var dialogupdate = Dialog(context)
-            var b = UpdatedialogBinding.inflate(LayoutInflater.from(context))
-            dialogupdate.setContentView(b.root)
-
-
-//                holder.binding.apply {
-//                    list.get(position).apply {
-//                        b.edtdate.setText(txtdate.text)
-//                        b.edttime.setText(txttime.text)
-//                        b.edtTask.setText(txttitle.text)
-//                        b.edtdescription.setText(txtdiscription.text)
-//
-//
-//                    }
-//                }
-
-            b.edtdate.setOnClickListener {
-
-                var date = Date()
-
-                var format1 = SimpleDateFormat("dd-MM-YYYY")
-                var currentDate = format1.format(date)
-
-                var dates = currentDate.split("-")
-                b.edtdate.text = currentDate
-
-                var dialog =
-                    DatePickerDialog(context, object : DatePickerDialog.OnDateSetListener {
-                        override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
-
-                            var Year = p1
-                            var Month = p2 + 1
-                            var Date = p3
-
-                            var selectedDate = "$p3-${(p2 + 1)}-$p1"
-                            b.edtdate.text = selectedDate
-                        }
-
-                    }, dates[2].toInt(), dates[1].toInt() - 1, dates[0].toInt())
-                dialog.show()
-            }
-
-            b.edttime.setOnClickListener {
-
-                var date = Date()
-
-                var format2 = SimpleDateFormat("hh:mm a")
-                var currentTime = format2.format(date)
-
-                b.edttime.text = currentTime
-                var seleTime = currentTime
-                var dialog1 =
-                    TimePickerDialog(context, object : TimePickerDialog.OnTimeSetListener {
-                        override fun onTimeSet(p0: TimePicker?, p1: Int, p2: Int) {
-
-                            var hour = p1
-                            var minute = p2
-                            var am_pm = if (p1 < 12) "AM" else "PM"
-                            var sdf = SimpleDateFormat("hh:mm a", Locale.US)
-                            var tme = "$hour:$minute $am_pm"
-                            b.edttime.setText(tme)
-//                    var selectedTime = "$p1:$p2"
-//                    binding.edttime.text = selectedTime
-                        }
-
-                    }, 10, 0, true)
-                dialog1.show()
-            }
-
-            b.btnsubmit.setOnClickListener {
-
-                var title = b.edtTask.text.toString()
-                var text = b.edtdescription.text.toString()
-                var Date = b.edtdate.text.toString()
-                var Month = b.edtdate.text.toString()
-                var Year = b.edtdate.text.toString()
-                var hour = b.edttime.text.toString()
-                var minute = b.edttime.text.toString()
-                var format = SimpleDateFormat("dd-MM-YYYY hh:mm a")
-                var current = format.format(Date())
-
-                var data = TaskEnitiy(title,text,Date,Month,Year,hour,minute,list.get(position).id)
-
-                db.task().UpdateTask(data)
-
-                dialogupdate.dismiss()
-
-                update(db.task().GetTask())
-            }
-
-            dialogupdate.show()
-
-            holder.binding.imgdelete.setOnClickListener {
-
-                Toast.makeText(context, "Hiii123456789", Toast.LENGTH_SHORT).show()
-
-                db.task().DeleteTask(list.get(position))
-
-                update(db.task().GetTask())
-
             }
         }
     }
-    fun update(l: List<TaskEnitiy>) {
-        list = l as ArrayList<TaskEnitiy>
+
+    fun setFilteredList(mList: ArrayList<TaskEnitiy>) {
+        this.getTask = getTask
         notifyDataSetChanged()
     }
+
+    fun update(notes: List<TaskEnitiy>) {
+        this.getTask = notes as ArrayList<TaskEnitiy>
+        notifyDataSetChanged()
+    }
+
 }
-
-
-
-
-
-
--- app/src/main/java/com/kevin/taskmanagement/Fragment/HomeFragment.kt
+++ app/src/main/java/com/kevin/taskmanagement/Fragment/HomeFragment.kt
@@ -4,43 +4,80 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.recyclerview.widget.RecyclerView
 import com.kevin.taskmanagement.Adapter.TaskAdapter
 import com.kevin.taskmanagement.Database.RoomDB
 import com.kevin.taskmanagement.Enitiy.TaskEnitiy
+import com.kevin.taskmanagement.R
 import com.kevin.taskmanagement.databinding.FragmentHomeBinding
+import java.util.Locale
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
-    lateinit var adapter: TaskAdapter
     var Tasklist = ArrayList<TaskEnitiy>()
     lateinit var db: RoomDB
+    lateinit var adapter: TaskAdapter
+    var recyclerView: RecyclerView = binding.rcvtasklist
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater)
-
         db = RoomDB.init(context)
+        recyclerView = binding.rcvtasklist
+        recyclerView.adapter = adapter
 
+        var list = db.task().GetTask()
+        adapter = TaskAdapter(list as ArrayList<TaskEnitiy>)
 
+        binding.rcvtasklist.layoutManager = LinearLayoutManager(context)
+        binding.rcvtasklist.adapter = adapter
 
-        initview()
+        searchData()
+
+        adapter = TaskAdapter(Tasklist)
+        recyclerView.adapter = adapter
 
         return binding.root
     }
 
+    private fun searchData() {
 
-    private fun initview() {
-        var list = db.task().GetTask()
-        adapter = TaskAdapter(
-            list as ArrayList<TaskEnitiy>
-        )
-        binding.rcvtasklist.layoutManager = LinearLayoutManager(context)
-        binding.rcvtasklist.adapter = adapter
+//        var getTask : ArrayList<TaskEnitiy>
+//        var gettask =getTask
+//            binding.tasksearch.setOnQueryTextListener(object : OnQueryTextListener{
+//                override fun onQueryTextSubmit(query: String?): Boolean {
+//                    return false
+//                }
+//
+//                override fun onQueryTextChange(newText: String?): Boolean {
+//                    filteredList(newText)
+//                    return true
+//                }
+//
+//                private fun filteredList(query: String?) {
+//                    if(query != null) {
+//                        var filteredList = ArrayList<TaskEnitiy>()
+//                        for (i in getTask) {
+//                            if (i.title.lowercase(Locale.ROOT).contains(query)) {
+//                                filteredList.add(i)
+//                            }
+//                        }
+//
+//                        if (filteredList.isEmpty()) {
+//                            Toast.makeText(context, "Enter valid data...", Toast.LENGTH_SHORT).show()
+//                        } else {
+//                            adapter.setFilteredList(filteredList)
+//                        }
+//                    }
+//                }
+//
+//            })
     }
-
 }
